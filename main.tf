759b4a6256eb83b197d3ced5a00f01b507ab8505
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-22 17:51:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 provider "aws" {
-  region = "us-west-2"
-  shared_credentials_files = ["~/.aws/credentials"]
+  region = "us-west-2"  
 }
 
 resource "aws_instance" "aws-jenkins-ec2" {
