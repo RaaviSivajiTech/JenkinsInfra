c42764088b1fa647ef26796a280d3dca330edde8
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-22 18:21:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,37 +24,3 @@ user_data = <<-EOF
   EOF
 }
 
-resource "aws_security_group" "jenkins_sg" {
-  name = "jenkins-sg"
-  vpc_id = "vpc-0507ccbd8f209cc85"
-
- ingress  {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
- }
-
- ingress  {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
- }
-
- ingress  {
-    description = "Incoming 443"
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
- }
-
- egress   {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
- }
-
-}
