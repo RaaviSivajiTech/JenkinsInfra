06b1b6c7d494775a1be3939f21f5e7e60126ddde
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-22 20:46:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,22 +5,21 @@ provider "aws" {
 resource "aws_instance" "aws-jenkins-ec2" {
   ami = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = "sg-0e243d83b5b1e420c"
+  
   tags = {
     Name = "Jenkins_Server"
   }
-
-user_data = <<-EOF
-#!bin/bash
-    sudo yum update -y
-    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
-    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-    sudo yum upgrade
-    sudo amazon-linux-extras install java-openjdk11 -y
-    sudo dnf install java-11-amazon-corretto -y
-    sudo yum install jenkins -y
-    sudo systemctl enable jenkins
-    sudo systemctl start jenkins
-  EOF
+  user_data = <<-EOF
+  #!bin/bash
+      sudo yum update -y
+      sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+      sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+      sudo yum upgrade
+      sudo amazon-linux-extras install java-openjdk11 -y
+      sudo dnf install java-11-amazon-corretto -y
+      sudo yum install jenkins -y
+      sudo systemctl enable jenkins
+      sudo systemctl start jenkins
+    EOF
 }
 
