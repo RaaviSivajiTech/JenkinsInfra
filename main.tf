b14b7096f038e3601fb8a686450259f2236cfda9
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-24 16:01:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 resource "aws_instance" "aws-jenkins-ec2" {
   ami = "ami-0c101f26f147fa7fd"
   instance_type = "t2.micro"
-  
+  metadata_options {
+    http_endpoint = enabled
+    http_tokens = required
+  }  
   tags = {
     Name = "Jenkins_Server"
   }
-  key_name = "sirisha"
-  metadata_options {
-    http_tokens = "required"
-  } 
+  key_name = "sirisha"  
 }
 
